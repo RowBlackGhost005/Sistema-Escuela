6c1c8273ed8804a809878f33d4607b02734e2dcf
Alfredo Valenzuela <jaime.valenzuela228249@potros.itson.edu.mx>
2023-05-02 23:23:20 -0700

    Método enviarMensaje
    El método enviar() se consume en la aplicación móvil y al momento en que el usuario envía un mensaje, ese mensaje se envía a una cola y luego un receptor lo cacha.

--- RESTNotificaciones/src/java/ws/NotificacionesResource.java
+++ RESTNotificaciones/src/java/ws/NotificacionesResource.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 /**
@@ -42,6 +43,31 @@ public class NotificacionesResource {
      */
     public NotificacionesResource() {
         
+    }
+    
+    @GET
+    @Path("/hola/{mensaje}")
+    public Response enviar(@PathParam("mensaje") String mensaje) throws IOException, TimeoutException {
+        // Crear la conexión y el canal
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection()) {
+            Channel channel = connection.createChannel();
+            
+            // Declarar la cola a la que se van a enviar los mensajes
+            channel.queueDeclare("hello", false, false, false, null);
+            
+            // Mensaje que se va a enviar
+            String message=mensaje;
+            
+            // Enviar el mensaje a la cola
+            channel.basicPublish("", "hello", null, message.getBytes("UTF-8"));
+            System.out.println("Enviado '" + message + "' a la cola '" + "hello" + "'");
+            
+            // Cerrar la conexión y el canal
+            channel.close();
+        }
+        return Response.status(200).build();
     }
 
     @GET
