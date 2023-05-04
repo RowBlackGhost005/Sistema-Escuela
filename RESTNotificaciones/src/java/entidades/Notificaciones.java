fdad539467065de318d03723500427f4d442840a
PaulinaCtz <paulina.cortez228557@potros.itson.edu.mx>
2023-05-03 18:46:57 -0700

    API enviar notificacion

--- RESTNotificaciones/src/java/entidades/Notificaciones.java
+++ RESTNotificaciones/src/java/entidades/Notificaciones.java
@@ -4,6 +4,15 @@
  */
 package entidades;
 
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
 /**
  *
  * @author Paulina Cortez Alamilla.
@@ -23,27 +32,31 @@ public class Notificaciones {
         this.destinatario = destinatario;
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
+    public void enviarNotificacion(String notificacion, String destinatario, String remitente) throws IOException, TimeoutException{
+         // Crear una conexión y un canal a RabbitMQ
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+        // Crear los headers
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("destinatario", destinatario);
+        headers.put("origen", remitente);
 
-    public String getMensaje() {
-        return mensaje;
-    }
+        // Crear la cola si no existe
+        String cola = "envio_modulo_notificaciones";
+        boolean durable = true;
+        boolean exclusive = false;
+        boolean autoDelete = false;
+        channel.queueDeclare(cola, durable, exclusive, autoDelete, null);
 
-    public void setMensaje(String mensaje) {
-        this.mensaje = mensaje;
-    }
+        // Publicar el mensaje en la cola
+        channel.basicPublish("", cola, new AMQP.BasicProperties.Builder().headers(headers).build(), notificacion.getBytes("UTF-8"));
+        System.out.println("Mensaje publicado en la cola: " + notificacion);
 
-    public String getDestinatario() {
-        return destinatario;
-    }
-
-    public void setDestinatario(String destinatario) {
-        this.destinatario = destinatario;
+        // Cerrar la conexión y el canal
+        channel.close();
+        connection.close();
     }
 }
