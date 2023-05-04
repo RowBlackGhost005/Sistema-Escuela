cc5192341e528e1add4947bfc5cc58024a5f2927
Alfredo Valenzuela <jaime.valenzuela228249@potros.itson.edu.mx>
2023-05-03 19:24:06 -0700

    Recibir mensajes en la app desde fuera

---
+++ Mensajeria/src/main/java/receptor/Receptor.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package receptor;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.Delivery;
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ *
+ * @author jvale
+ */
+public class Receptor {
+
+    String nombre_cola;
+    String mensaje;
+
+    public Receptor(String nombre_cola, String mensaje) {
+        this.nombre_cola = nombre_cola;
+        this.mensaje = mensaje;
+    }
+
+    public Receptor() {
+    }
+
+    public String recibirMensaje(String nombreCola) throws IOException, TimeoutException, ExecutionException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(nombreCola, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        final CompletableFuture<String> completableFuture = new CompletableFuture<>();
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            completableFuture.complete(message);
+        };
+
+        channel.basicConsume(nombreCola, true, deliverCallback, consumerTag -> {
+        });
+
+        try {
+            return completableFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException("Error al recibir mensaje de la cola " + nombreCola, e);
+        }
+    }
+
+    public String getNombre_cola() {
+        return nombre_cola;
+    }
+
+    public void setNombre_cola(String nombre_cola) {
+        this.nombre_cola = nombre_cola;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+}
--- RESTNotificaciones/src/java/ws/NotificacionesResource.java
+++ RESTNotificaciones/src/java/ws/NotificacionesResource.java
@@ -12,12 +12,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import com.rabbitmq.client.*;
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import productor.Productor;
+import receptor.Receptor;
 
 /**
  * REST Web Service
@@ -44,6 +46,13 @@ public class NotificacionesResource {
      */
     public NotificacionesResource() {
         
+    }
+    
+    @GET
+    @Path("/recibir/{cola}")
+    public String recibir(@PathParam("cola")String cola) throws IOException, TimeoutException, ExecutionException{
+        Receptor receptor = new Receptor();
+        return receptor.recibirMensaje(cola);
     }
     
     @GET
